d2f2f6f36e54fae2c1a4f22a5ec39494f47bf29e
Leo Chen <leo.chen.shc@gmail.com>
2022-12-02 00:13:54 -0800

    added more icons and updated the comp layout

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Post.scss";
+import likesIcon from "../../assets/icons/heart.svg";
+import commentsIcon from "../../assets/icons/chat_bubble.svg";
+import ApexGameImg from "../../assets/images/image 4.png";
 
 const post = () => {
   return (
-    <section className="post">
-      <div className="post__header">
+    <main className="post">
+      <section className="post__header">
         <ol className="post__header-list">
           <li className="post__header-list-item">Popular</li>
           <li className="post__header-list-item">Gallery</li>
@@ -13,32 +16,143 @@ const post = () => {
           <li className="post__header-list-item">Guides</li>
           <li className="post__header-list-item">Reviews</li>
         </ol>
-      </div>
-      <div className="post__items-wrap">
-        <div className="post__items">
-          <img src="" alt="apex gaming images" />
+      </section>
+      <section className="post__items-wrap">
+        <div className="post__items-hero">
+          <img
+            className="post__items-img"
+            src={ApexGameImg}
+            alt="apex gaming images"
+          />
           <div className="post__items-info-bar">
             <p className="post__items-username">Apex-Huntress</p>
             <div className="post__items-comments-likes-wrap">
               <img
-                src=""
+                src={commentsIcon}
                 alt="comments icon"
-                className="post__items-commentsIcon"
+                className="post__items-comments-icon"
               />
-              <p className="post__items-comment-count">9.3k</p>
-              <img src="" alt="likes icon" className="post__items-likesIcon" />
+              <p className="post__items-comments-count">9.3k</p>
+              <img
+                src={likesIcon}
+                alt="likes icon"
+                className="post__items-likes-icon"
+              />
               <p className="post__items-likes-count">1.8k</p>
             </div>
           </div>
         </div>
-        <div className="post__items"></div>
-        <div className="post__items"></div>
-        <div className="post__items"></div>
-        <div className="post__items"></div>
-        <div className="post__items"></div>
-        <div className="post__items"></div>
-      </div>
-    </section>
+
+        <aside className="post__additional-info">
+          <div className="post__additional-info-find-friends">Find Friends</div>
+          <div className="post__additional-info-find-base">
+            Find Base and Hub
+          </div>
+          <div className="post__additional-info-popular-hubs">Popular Hubs</div>
+        </aside>
+
+        <div className="post__items">
+          <img className="post__items-img" src="" alt="gaming images" />
+          <div className="post__items-info-bar">
+            <p className="post__items-username">User Name</p>
+            <div className="post__items-comments-likes-wrap">
+              <img
+                src={commentsIcon}
+                alt="comments icon"
+                className="post__items-comments-icon"
+              />
+              <p className="post__items-comments-count">##</p>
+              <img
+                src={likesIcon}
+                alt="likes icon"
+                className="post__items-likes-icon"
+              />
+              <p className="post__items-likes-count">##</p>
+            </div>
+          </div>
+        </div>
+        <div className="post__items">
+          <img className="post__items-img" src="" alt=" gaming images" />
+          <div className="post__items-info-bar">
+            <p className="post__items-username">User Name</p>
+            <div className="post__items-comments-likes-wrap">
+              <img
+                src={commentsIcon}
+                alt="comments icon"
+                className="post__items-comments-icon"
+              />
+              <p className="post__items-comments-count">##</p>
+              <img
+                src={likesIcon}
+                alt="likes icon"
+                className="post__items-likes-icon"
+              />
+              <p className="post__items-likes-count">##</p>
+            </div>
+          </div>
+        </div>
+        <div className="post__items">
+          <img className="post__items-img" src="" alt=" gaming images" />
+          <div className="post__items-info-bar">
+            <p className="post__items-username">User Name</p>
+            <div className="post__items-comments-likes-wrap">
+              <img
+                src={commentsIcon}
+                alt="comments icon"
+                className="post__items-comments-icon"
+              />
+              <p className="post__items-comments-count">##</p>
+              <img
+                src={likesIcon}
+                alt="likes icon"
+                className="post__items-likes-icon"
+              />
+              <p className="post__items-likes-count">##</p>
+            </div>
+          </div>
+        </div>
+        <div className="post__items">
+          <img className="post__items-img" src="" alt=" gaming images" />
+          <div className="post__items-info-bar">
+            <p className="post__items-username">User Name</p>
+            <div className="post__items-comments-likes-wrap">
+              <img
+                src={commentsIcon}
+                alt="comments icon"
+                className="post__items-comments-icon"
+              />
+              <p className="post__items-comments-count">##</p>
+              <img
+                src={likesIcon}
+                alt="likes icon"
+                className="post__items-likes-icon"
+              />
+              <p className="post__items-likes-count">##</p>
+            </div>
+          </div>
+        </div>
+        <div className="post__items">
+          <img className="post__items-img" src="" alt=" gaming images" />
+          <div className="post__items-info-bar">
+            <p className="post__items-username">User Name</p>
+            <div className="post__items-comments-likes-wrap">
+              <img
+                src={commentsIcon}
+                alt="comments icon"
+                className="post__items-comments-icon"
+              />
+              <p className="post__items-comments-count">##</p>
+              <img
+                src={likesIcon}
+                alt="likes icon"
+                className="post__items-likes-icon"
+              />
+              <p className="post__items-likes-count">##</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
