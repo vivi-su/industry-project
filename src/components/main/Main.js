ad54c9a09500ab2b23d64261cee00ab965eda4d7
Vivi Schulz <haweirh@gmail.com>
2022-12-01 22:51:21 -0800

    Added model

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,11 @@
+import Hero from "../hero/Hero";
+import Post from "../posts/Post";
 export default function Main (){
 
-    return(
+    return (
+      <>
         <Hero />
+        <Post />
+      </>
     );
 }
