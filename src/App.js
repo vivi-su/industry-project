a497c0c9ad3388cc7e6197aece93aff8d9b536b2
Vivi Schulz <haweirh@gmail.com>
2022-12-01 19:57:16 -0800

    Basic header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from "../src/components/header/Header";
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
